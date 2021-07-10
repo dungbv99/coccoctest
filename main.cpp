1ae080ee59f496842b592652bfede2ee98b80199
matdieuhau999 <matdieuhau999@gmail.com>
2021-07-10 11:22:15 +0700

    remove my defind min heap data struct and use priority queue

--- main.cpp
+++ main.cpp
@@ -2,112 +2,112 @@
 #include <sys/stat.h>
 using namespace std;
 
-uint64_t limitMemory=1000000;
+uint64_t limitMemory;
+
 //when program run, will incur extra memory like stack to store recursive when sort array
 //so don't read use limitMemory to read from input file to sort
 uint64_t calculateSize(){
-    return limitMemory;
+    return limitMemory/1.25;
 }
 
-struct MyPair{
-    int id;
-    string value;
-};
-
+/*
+ * receive a vector<string>
+ * sort it and write to file with index of file is file name
+ */
 void writeSortToFile(vector<string>& listLine, string outFilePath){
     ofstream outFile(outFilePath);
     sort(listLine.begin(), listLine.end());
     ostream_iterator<string> output_iterator(outFile, "\n");
+    /*
+     * write n-1 line with "\n" in the end of file
+     * and write nth file without "\n"
+     * to guarantee when merge file the exception is not happened
+     */
     copy(listLine.begin(), listLine.begin()+listLine.size()-1, output_iterator);
     outFile << listLine[listLine.size()-1];
     outFile.close();
     return;
 }
-//split input file to n file with n
-//size of 1 file is approximately calculateSize()
-//n = size of input file / calculateSize()
+
+/*
+ * depend on limited memory for read input file
+ * when reading an appropriate memory size, go writeSortToFile function to sort and write to file
+ * return number of file to be split and sorted
+ */
 int divideSortFile(string inputPath){
     ifstream inFile(inputPath);
     string line;
     vector<string> listLine;
     uint64_t size = 0;
-    int i = 0;
+    int n = 0;
     mkdir("file", 0777);
-//    int num = 0;
     while(getline(inFile, line)){
         listLine.push_back(line);
         size += line.size();
-//        num++; // consider num/log(num)
         if(size > limitMemory){
-
-            writeSortToFile(listLine, "./file/"+to_string(i)+".txt");
+            writeSortToFile(listLine, "./file/" + to_string(n) + ".txt");
             size = 0;
-//            num = 0;
             listLine.clear();
             listLine.shrink_to_fit();
-            cout << "i " << i << "\n";
-            cout << listLine.size() << "\n";
-            i++;
+            n++;
         }
     }
+    //the redundancy of input file
     if(!listLine.empty()){
-        writeSortToFile(listLine, "./file/"+to_string(i)+".txt");
+        writeSortToFile(listLine, "./file/" + to_string(n) + ".txt");
         listLine.clear();
-        i++;
+        n++;
     }
-    return i;
+    return n;
 }
 
-//after first element in list is writen in out put file, heapify list to get the smallest element
-void heapify(vector<MyPair> &list, int n, int i){
-    int min = i;
-    int l = 2*i+1;
-    int r = 2*i+2;
-    if(l < n && list[l].value < list[min].value ){
-        min = l;
-    }
-    if(r < n && list[r].value < list[min].value){
-        min = r;
-    }
-    if(min != i){
-        swap(list[i], list[min]);
-        heapify(list, n, min);
-    }
-}
-//build the first heap in the first time
-void buildHeap(vector<MyPair> &list, int n){
-    for(int i = n/2-1; i >=0; i--){
-        heapify(list,n,i);
-    }
-}
+/*
+ * defind pi type with:
+ *  -   pi.first is the line value in priority queue
+ *  -   pi.second is the index of file
+ */
+typedef pair<string, int> pi;
 
+/*
+ * merge n sorted file to output file
+ */
 void mergeFile(int n, string outPath){
-    cout << "merge \n";
-    vector<ifstream> listFile(n);
-    vector<MyPair> list(n);
+    vector<ifstream> listSortedFile(n);
+    /*
+     * use priority queue with greater comparator for the first element is always the smallest of the elements it contains
+     * pi.first is string which will be used to compare
+     */
+    priority_queue<pi, vector<pi>, greater<pi>> pq;
+    string line;
     for(int i=0; i < n; i++){
-        listFile[i].open("./file/"+to_string(i)+".txt");
-        getline(listFile[i], list[i].value);
-        list[i].id = i;
+        listSortedFile[i].open("./file/" + to_string(i) + ".txt");
+        getline(listSortedFile[i], line);
+        pq.push({line, i});
     }
-
-    buildHeap(list, n);
-
-
     ofstream outFile(outPath);
-
-    while (n>0){
-        heapify(list, n, 0);
-        outFile << list[0].value << "\n";
-        if(listFile[list[0].id].eof()){
+    while (n>0){ // while exist file is open
+        pi minNode = pq.top();
+        pq.pop();
+        outFile << minNode.first << "\n";
+        if(listSortedFile[minNode.second].eof()){ //if the file is EOF, close file and decrease n
             n--;
-            listFile[list[0].id].close();
-            list.erase(list.begin());
-            buildHeap(list, n);
-        }else{
-            getline(listFile[list[0].id],list[0].value);
+            listSortedFile[minNode.second].close();
+        }else{ //push the next line of file to pq
+            getline(listSortedFile[minNode.second], line);
+            pq.push({line, minNode.second});
         }
     }
+}
+
+/*
+ * execute external sort function
+ * divide 2 step
+ * step 1: divide input file to n file depend on limit memory and sort them
+ * step 2: merge n sorted file
+ */
+void externalSort(string inputPath, string outputPath){
+    int n = divideSortFile(inputPath);
+    mergeFile(n, outputPath);
 }
 
 //compare all 2 adjacent line in output file
@@ -138,10 +138,10 @@ int main(){
     cin >> outputPath;
     cout << "limitMemory(byte): \n";
     cin >> limitMemory;
+
     time_t start, finish;
     time(&start);
-    int n = divideSortFile(inputPath);
-    mergeFile(n, outputPath);
+    externalSort(inputPath, outputPath);
     time(&finish);
     cout << "sort time = " << difftime(finish, start) << "seceonds \n" ;
 //    checkOutputSortFile(outputPath);
