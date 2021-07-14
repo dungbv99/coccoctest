e970c02c2bc434dec42745be61a1fef6d564f427
dungbv99 <42971823+dungbv99@users.noreply.github.com>
2021-07-14 09:48:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -75,7 +75,7 @@ void mergeFile(int n, string outPath){
     vector<ifstream> listSortedFile(n);
     /*
      * use priority queue with greater comparator for the first element is always the smallest of the elements it contains
-     * pi.first is string which will be used to compare
+     * pi.first(the value of thí line) is string which will be used to compare before pi.second(id of sorted file)
      */
     priority_queue<pi, vector<pi>, greater<pi>> pq;
     string line;
